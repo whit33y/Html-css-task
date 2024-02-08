ae5940b07a3c7c189eb54da447f0e93e78ae7c7e
whit33y <troppout@gmail.com>
2024-02-08 12:01:34 +0100

    Search component to reduce code in nav

--- app/ui/nav/nav.js
+++ app/ui/nav/nav.js
@@ -1,15 +1,15 @@
 import styles from "./nav.module.css";
-import Image from "next/image";
 import BuyButton from "../buttons/buybutton";
 import WatchButton from "../buttons/watchbutton";
+import Search from "../Search/search";
 
 function Navigation() {
     return (
         <nav className={styles.nav}>
             <div className={styles.upperNav}>
                 <div>
-                    <a href="/">
-                        <Image src="/applelogo.png" alt="Apple logo" width={32} height={32} />
+                    <a href="/" >
+                        <img src="/applelogo.png" alt="Apple logo" className={styles.appleLogo} />
                     </a>
                 </div>
                 <div>
@@ -19,11 +19,7 @@ function Navigation() {
                         <li><a href='/contact'>Contact</a></li>
                     </ol>
                 </div>
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={styles.nav} width={32} height={32} color="#FFFFF0">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                    </svg>
-                </div>
+                <Search />
             </div>
             <div className={styles.bottomNav}>
                 <div className={styles.rainbowText}>
