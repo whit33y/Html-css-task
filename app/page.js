b5646178ca1a13e87f67299849473d62da9312ed
whit33y <troppout@gmail.com>
2024-02-08 15:26:17 +0100

    React icons, style changes and slider added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import styles from "./page.module.css";
 import SpecsButton from "./ui/buttons/specsbutton";
+import Slider from "./ui/slider/slider";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Virtual reality was never this perfect <br /> and it will never be the same</h1>
+      <h1 className={styles.title}>Virtual reality was never this <span className={styles.textOrange}>perfect</span> <br /> and it will never be the same</h1>
       <div className={styles.content}>
         <div className={styles.imageConteiner}>
           <img src="/visionproblack.jpeg" alt="Apple Vision Pro" className={styles.image} />
@@ -15,7 +16,7 @@ export default function Home() {
         <h2 className={styles.headerText}>The future is now</h2>
         <p>
           Our new product is a powerful revolution of our decade. <br />
-          Years of experience have allowed us to create Apple Vision Pro. <br />
+          Years of experience have allowed us to create <span className={styles.textOrange}>Apple Vision Pro. </span><br />
           A wearable device that changes the way we look at entertaiment, work and many more...< br />
           Transform any room into office, cinema, game center or just whatever you want...
         </p>
@@ -28,21 +29,31 @@ export default function Home() {
         allowFullScreen
         className={styles.videoFrame}
       />
+      <div className={styles.bottomMeeting}>
+        <h2>Online meeting was never this productive</h2>
+        <p>Connect with Facetime or other aplication wherever you are and enjoy your meeting. <br />
+          Colaborate with your cooworkers on the same documents. Share apps, windows or your entire view.
+        </p>
+        <Slider />
+      </div>
+      <h2 className={styles.bottomGirlText}>Comfortable for every had shape</h2>
       <div className={styles.bottomGirlImage}>
-        <img src="/visiongirl.jpeg" alt="Apple Vision Pro" />
+        <img src="/visiongirl.jpeg" alt="Girl wearing Apple Vision Pro" />
       </div>
       <div className={styles.bottomContent}>
         <div className={styles.bottomContentText}>
           <h2 className={styles.headerText}>With attention to detail in every single component<br />
-            Precision and passion make us the <span className={styles.headerTextOrange}>best</span></h2>
+            Precision and passion make us the <span className={styles.textOrange}>best</span></h2>
           <h2>Just take a <span className={styles.headerTextRainbow}>look...</span></h2>
         </div>
         <div className={styles.bottomContentImg}>
-          <img src="/appletop.jpeg" alt="Apple Vision Pro" />
-          <img src="/appletop2.jpeg" alt="Apple Vision Pro" />
+          <img src="/appletop.jpeg" alt="Apple Vision Pro Glass" />
+          <img src="/appletop2.jpeg" alt="Apple Vision Pro Glass" />
         </div>
       </div>
       <SpecsButton />
+
+
     </main >
   );
 }
---
+++ app/ui/slider/slider.js
@@ -0,0 +1,77 @@
+"use client";
+import React from "react";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
+import "./slider.css";
+
+const Slider = () => {
+    const filteredItems = [
+        {
+
+            img: "videoframe5.png",
+
+
+        },
+        {
+
+            img: "videoframe.png",
+
+
+        },
+        {
+
+            img: "videoframe2.png",
+
+
+        },
+        {
+
+            img: "videoframe4.png",
+
+        },
+        {
+
+            img: "videoframe3.png",
+
+
+        },
+    ];
+
+    const slideLeft = () => {
+        let slider = document.getElementById("slider");
+        slider.scrollLeft = slider.scrollLeft - 235;
+    };
+
+    const slideRight = () => {
+        let slider = document.getElementById("slider");
+        slider.scrollLeft = slider.scrollLeft + 235;
+    };
+    return (
+        <>
+            <div className="trending">
+                <div className="container">
+                    <div className="title-btns">
+                        <h3></h3>
+                        <div className="btns">
+                            <button title="scroll left" onClick={slideLeft}>
+                                <AiOutlineArrowLeft />
+                            </button>
+                            <button title="scroll right" onClick={slideRight}>
+                                <AiOutlineArrowRight />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="row-container" id="slider">
+                        {filteredItems.map((item) => (
+                            <div key={item.img} className="row-item">
+                                <div className="item-header">
+                                    <img src={item.img} alt="Vision Pro meeting" />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+export default Slider;
