a0f8b0a268fcb8c07056ee2d26858b4e58d89bcd
whit33y <troppout@gmail.com>
2024-02-08 22:03:24 +0100

    Button style changes

--- app/ui/buttons/specsbutton.js
+++ app/ui/buttons/specsbutton.js
@@ -2,13 +2,12 @@ import "./button.css";
 
 function SpecsButton() {
     return (
-        <a href='/specs'>
-            <button className="specsButton">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" width={24} height={24}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                </svg>
-                Learn more
-            </button>
+        <a href='/specs' className="specsButton">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" width={24} height={24}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+            Learn more
+
         </a>
     );
 }
