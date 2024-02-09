04bf82796e0101aba1a8327ec7a2d1ff8994db89
whit33y <troppout@gmail.com>
2024-02-09 15:29:55 +0100

    Contact page classname changes

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,16 +2,16 @@ import "./page.css";
 
 function Page() {
     return (
-        <main className="main">
-            <h1 className="title">Feel free to contact us</h1>
-            <div className="content">
-                <div className="map">
+        <main className="container">
+            <h1 className="container-title">Feel free to contact us</h1>
+            <div className="container-top">
+                <div className="container-top-map">
                     <h2>Our headquaters- Apple Park</h2>
-                    <img src="/map.png" alt="Apple Park Cupertino on map" className="mapImage" />
+                    <img src="/map.png" alt="Apple Park Cupertino on map" />
                     <a href="https://maps.app.goo.gl/yR7cBXuWCFTChqSD9" target="_blank"><p>One Apple Park Way, Cupertino, CA 95014, USA 📌-press to view on map</p></a>
                     <p>Visit us 7 days a week from 9:00am to 6:00pm</p>
                 </div>
-                <div className="contentRight">
+                <div className="container-top-contact">
 
                     <h2>Call us</h2>
                     <p>Apple Enterprise Sales (877) 412–7753</p>
@@ -27,11 +27,11 @@ function Page() {
                     <p>resellers@apple.com</p>
                 </div>
             </div>
-            <div className="contentBottom">
+            <div className="container-bottom">
                 <h2>Feedback</h2>
-                <p className="contentBottomText">Write an e-mail or call us about what we can change on our website or in our products</p>
-                <p className="contentBottomDetails">feedback@apple.com</p>
-                <p className="contentBottomDetails">001–800–692–7753</p>
+                <p className="container-bottom-text">Write an e-mail or call us about what we can change on our website or in our products</p>
+                <p className="container-bottom-text-details">feedback@apple.com</p>
+                <p className="container-bottom-text-details">001–800–692–7753</p>
             </div>
         </main>
     );
