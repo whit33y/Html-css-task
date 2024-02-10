da0564c39e6b1a6d98f85a4d046e4141c1dc52c5
whit33y <troppout@gmail.com>
2024-02-10 14:19:33 +0100

    Style changes

--- app/ui/Search/search.js
+++ app/ui/Search/search.js
@@ -4,7 +4,7 @@ function Search() {
     return (
         <div className="search">
             <input type="text" placeholder="Search..." maxLength={15} />
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width={32} height={32} color="#FFFFF0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width={32} height={32} color="#FFFFF0" className="glass">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
         </div>
